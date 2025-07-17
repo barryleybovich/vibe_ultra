d35436a64768b429c945dad17f283fe06834fdcb
barryleybovich <bleybovich@outlook.com>
2025-07-16 21:49:18 -0400

    Updated TrainingPlanParser.tsx

--- src/components/TrainingPlanParser.tsx
+++ src/components/TrainingPlanParser.tsx
@@ -402,7 +402,7 @@ export const TrainingPlanParser: React.FC<TrainingPlanParserProps> = ({
                                 <span className={`text-xs font-medium ${
                                   isLowForm ? 'text-red-700' : 'text-blue-700'
                                 }`}>
-                                  {workout.form.toFixed(2)}
+                                  {(todaysWorkout.form * 100).toFixed(0)}%
                                 </span>
                               </div>
                             </div>
